0f2ad07a71b9b9c89a0eb5f8d975e6e4cd651cc5
Vance <muchongo.vance@gmail.com>
2023-12-13 14:09:28 +0000

    initial commit

---
+++ pull.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * pall - prints all the values on the stack
+ * @stack: double pointer to the head of the stack
+ * @line_number: line number being executed
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+    /* Implement the logic for pall opcode here */
+    /* Example:
+    if (*stack == NULL) {
+        return; // Stack is empty, nothing to print
+    }
+
+    stack_t *current = *stack;
+    while (current != NULL) {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
+    */
+}
---
+++ push.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * push - pushes an element onto the stack
+ * @stack: double pointer to the head of the stack
+ * @line_number: line number being executed
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+    /* Implement the logic for push opcode here */
+    /* Example:
+    if (/* condition to check if argument exists and is an integer */) {
+        /* Convert argument to integer and push onto stack */
+    } else {
+        fprintf(stderr, "L%u: usage: push integer\n", line_number);
+        /* Handle error and exit */
+    }
+}
