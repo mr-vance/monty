e30d7bbc4f6793ecd9be999e11901664795d9dea
Vance <muchongo.vance@gmail.com>
2023-12-16 16:46:16 +0000

    monty.c

--- monty.h
+++ monty.h
@@ -37,7 +37,7 @@ typedef struct instruction_s
 /* Function prototypes */
 
 /* Opcode functions */
-void push(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, int value, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 
 /* Other helper functions */
