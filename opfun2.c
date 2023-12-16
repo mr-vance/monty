aa5f4e644f3edaf2c94a7e60358f06f49aafd3bc
Vance <muchongo.vance@gmail.com>
2023-12-16 17:25:45 +0000

    Revert "alterations"
    This reverts commit a5b675ea54f5d48c14388d453dcfed0172cd49e8.

--- opfun2.c
+++ opfun2.c
@@ -1,72 +1,82 @@
 #include "monty.h"
 
 /**
- * swap_elements - Swap the top two elements of the stack
- * @stack: Double linked list representing the stack
- * @line_number: Line number in the file
+ * swap - Delete top of list
+ * @stack: Double linked list
+ * @line_number: File line execution
  */
-void swap_elements(stack_t **stack, unsigned int line_number) {
-    if (!*stack || !(*stack)->next) {
-        fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
-        free_resources();
-        exit(EXIT_FAILURE);
-    }
+void swap(stack_t **stack, unsigned int line_number)
+{
+	int tmp;
 
-    int temp = (*stack)->value;
-    (*stack)->value = (*stack)->next->value;
-    (*stack)->next->value = temp;
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n",
+			line_number);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+	tmp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = tmp;
 }
 
 /**
- * add_elements - Add the top two elements of the stack
- * @stack: Double linked list representing the stack
- * @line_number: Line number in the file
+ * add - add the top two elements of the stack
+ * @stack: Double linked list
+ * @line_number: File line execution
  */
-void add_elements(stack_t **stack, unsigned int line_number) {
-    if (!*stack || !(*stack)->next) {
-        fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
-        free_resources();
-        exit(EXIT_FAILURE);
-    }
-
-    (*stack)->next->value += (*stack)->value;
-    pop(stack, line_number);
+void add(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n",
+			line_number);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n = (*stack)->next->n + (*stack)->n;
+	pop(stack, line_number);
 }
 
 /**
- * subtract_elements - Subtract the top two elements of the stack
- * @stack: Double linked list representing the stack
- * @line_number: Line number in the file
+ * sub - substract the top two elements of the stack
+ * @stack: Double linked list
+ * @line_number: Line counter
  */
-void subtract_elements(stack_t **stack, unsigned int line_number) {
-    if (!*stack || !(*stack)->next) {
-        fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
-        free_resources();
-        exit(EXIT_FAILURE);
-    }
-
-    (*stack)->next->value -= (*stack)->value;
-    pop(stack, line_number);
+void sub(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n",
+			line_number);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n = (*stack)->next->n - (*stack)->n;
+	pop(stack, line_number);
 }
 
 /**
- * divide_elements - Divide the top two elements of the stack
- * @stack: Double linked list representing the stack
- * @line_number: Line number in the file
+ * divi - divide the top two elements of the stack
+ * @stack: Double linked list
+ * @line_number: File line counter
  */
-void divide_elements(stack_t **stack, unsigned int line_number) {
-    if (!*stack || !(*stack)->next) {
-        fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
-        free_resources();
-        exit(EXIT_FAILURE);
-    }
-
-    if ((*stack)->value == 0) {
-        fprintf(stderr, "L%u: division by zero\n", line_number);
-        free_resources();
-        exit(EXIT_FAILURE);
-    }
-
-    (*stack)->next->value /= (*stack)->value;
-    pop(stack, line_number);
+void divi(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n",
+			line_number);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n = (*stack)->next->n / (*stack)->n;
+	pop(stack, line_number);
 }
